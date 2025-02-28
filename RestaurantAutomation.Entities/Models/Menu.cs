492eaf5f8c06e003e2d04ededf7f7c9415b0a053
omrkucuk <khuseyinomer@gmail.com>
2025-02-28 17:11:56 +0300

    Revert "Validator edit"
    This reverts commit d4a0b1fd1e210a6092b2194de7d488cf0cada554.

---
+++ RestaurantAutomation.Entities/Models/Menu.cs
@@ -0,0 +1,10 @@
+﻿using RestaurantAutomation.Entities.Abstractions;
+
+namespace RestaurantAutomation.Entities.Models
+{
+    public class Menu : Entity
+    {
+        public string Name { get; set; }
+        public virtual ICollection<MenuItem> MenuItems { get; set; }
+    }
+}
